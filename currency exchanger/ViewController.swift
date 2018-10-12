9ce4b27a09a8de6f314e88a24ccd4cd99822d844
Sebastian Berglönn <sebastianberglonn@gmail.com>
2018-10-12 08:50:12 +0200

    Initial Commit

---
+++ currency exchanger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  currency exchanger
+//
+//  Created by Sebastian Berglönn on 2018-10-12.
+//  Copyright © 2018 Sebastian Berglönn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
