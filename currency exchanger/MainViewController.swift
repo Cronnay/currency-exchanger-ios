87fd6e22f2f9d97458638adcc18a864b2f84675b
Sebastian Berglönn <sebastianberglonn@gmail.com>
2018-10-24 16:15:03 +0200

    Moved the getRatesFortoday to MainViewController to be able to handle the closure there

--- currency exchanger/MainViewController.swift
+++ currency exchanger/MainViewController.swift
@@ -2,10 +2,24 @@ import UIKit
 import Foundation
 
 class MainViewController: UIViewController {
+    // Defines the variables
+    var rates: Rate?
+    var Rates: URLBuilder?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let Rates = URLBuilder()
-        print(Rates.getRatesforToday())
+        // When view has been loaded, Rates will create a new instance of URLBuilder class
+        Rates = URLBuilder()
+        
+        // Gets the rates for today.
+        getRatesforToday()
+    }
+    public func getRatesforToday() -> Void {
+        let url: URL = Rates!.getURL("latest")
+        Rates!.createRequest(url: url) { (response: Rate?) in
+            if let resp = response {
+                self.rates = resp
+            }
+        }
     }
 }
